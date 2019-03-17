50e49b5fe74eee4989ffd866f13c4fdcfce170c8
Stefan Wojcik <swojcik@pewresearch.org>
2019-03-17 18:22:00 -0400

    finally got eff stats to work

--- notebooks/eff_stats.py
+++ notebooks/eff_stats.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 from math import pi
 
 
-def eff_stats(df_path = 'data/DataFiles/NCAATourneyDetailedResults.csv'):
+def eff_stats(df_path = 'data/DataFiles/RegularSeasonDetailedResults.csv'):
 	print("loading...")
 	df = pd.read_csv(df_path)
-
+	#
 	#Points Winning/Losing Team
 	df['WPts'] = df.apply(lambda row: 2*row.WFGM + row.WFGM3 + row.WFTM, axis=1)
 	df['LPts'] = df.apply(lambda row: 2*row.LFGM + row.LFGM3 + row.LFTM, axis=1)
-
+	#
 	#Calculate Winning/losing Team Possesion Feature
 	wPos = df.apply(lambda row: 0.96*(row.WFGA + row.WTO + 0.44*row.WFTA - row.WOR), axis=1)
 	lPos = df.apply(lambda row: 0.96*(row.LFGA + row.LTO + 0.44*row.LFTA - row.LOR), axis=1)
@@ -23,7 +23,7 @@ def eff_stats(df_path = 'data/DataFiles/NCAATourneyDetailedResults.csv'):
 	#(plus/minus one or two - depending on how quarters end)
 	#so let's just take the average
 	df['Pos'] = (wPos+lPos)/2
-
+	#
 	print("computing offensive/defensive rating...")
 	#Offensive efficiency (OffRtg) = 100 x (Points / Possessions)
 	df['WOffRtg'] = df.apply(lambda row: 100 * (row.WPts / row.Pos), axis=1)
@@ -34,7 +34,6 @@ def eff_stats(df_path = 'data/DataFiles/NCAATourneyDetailedResults.csv'):
 	#Net Rating = Off.Rtg - Def.Rtg
 	df['WNetRtg'] = df.apply(lambda row:(row.WOffRtg - row.WDefRtg), axis=1)
 	df['LNetRtg'] = df.apply(lambda row:(row.LOffRtg - row.LDefRtg), axis=1)
-	                         
 	#Assist Ratio : Percentage of team possessions that end in assists
 	df['WAstR'] = df.apply(lambda row: 100 * row.WAst / (row.WFGA + 0.44*row.WFTA + row.WAst + row.WTO), axis=1)
 	df['LAstR'] = df.apply(lambda row: 100 * row.LAst / (row.LFGA + 0.44*row.LFTA + row.LAst + row.LTO), axis=1)
@@ -43,46 +42,80 @@ def eff_stats(df_path = 'data/DataFiles/NCAATourneyDetailedResults.csv'):
 	#(TO * 100) / (FGA + (FTA * 0.44) + AST + TO)
 	df['WTOR'] = df.apply(lambda row: 100 * row.WTO / (row.WFGA + 0.44*row.WFTA + row.WAst + row.WTO), axis=1)
 	df['LTOR'] = df.apply(lambda row: 100 * row.LTO / (row.LFGA + 0.44*row.LFTA + row.LAst + row.LTO), axis=1)
-	                    
 	#The Shooting Percentage : Measure of Shooting Efficiency (FGA/FGA3, FTA)
 	df['WTSP'] = df.apply(lambda row: 100 * row.WPts / (2 * (row.WFGA + 0.44 * row.WFTA)), axis=1)
 	df['LTSP'] = df.apply(lambda row: 100 * row.LPts / (2 * (row.LFGA + 0.44 * row.LFTA)), axis=1)
 	#eFG% : Effective Field Goal Percentage adjusting for the fact that 3pt shots are more valuable 
-	df['WeFGP'] = df.apply(lambda row:(row.WFGM + 0.5 * row.WFGM3) / row.WFGA, axis=1)      
-	df['LeFGP'] = df.apply(lambda row:(row.LFGM + 0.5 * row.LFGM3) / row.LFGA, axis=1)   
+	df['WeFGP'] = df.apply(lambda row:(row.WFGM + 0.5 * row.WFGM3) / row.WFGA, axis=1)
+	df['LeFGP'] = df.apply(lambda row:(row.LFGM + 0.5 * row.LFGM3) / row.LFGA, axis=1)
 	#FTA Rate : How good a team is at drawing fouls.
 	df['WFTAR'] = df.apply(lambda row: row.WFTA / row.WFGA, axis=1)
 	df['LFTAR'] = df.apply(lambda row: row.LFTA / row.LFGA, axis=1)
-	                         
 	#OREB% : Percentage of team offensive rebounds
 	df['WORP'] = df.apply(lambda row: row.WOR / (row.WOR + row.LDR), axis=1)
 	df['LORP'] = df.apply(lambda row: row.LOR / (row.LOR + row.WDR), axis=1)
 	#DREB% : Percentage of team defensive rebounds
 	df['WDRP'] = df.apply(lambda row: row.WDR / (row.WDR + row.LOR), axis=1)
-	df['LDRP'] = df.apply(lambda row: row.LDR / (row.LDR + row.WOR), axis=1)                                      
+	df['LDRP'] = df.apply(lambda row: row.LDR / (row.LDR + row.WOR), axis=1)
 	#REB% : Percentage of team total rebounds
 	df['WRP'] = df.apply(lambda row: (row.WDR + row.WOR) / (row.WDR + row.WOR + row.LDR + row.LOR), axis=1)
 	df['LRP'] = df.apply(lambda row: (row.LDR + row.LOR) / (row.WDR + row.WOR + row.LDR + row.LOR), axis=1) 
-
-
-	df.drop(['WFGM', 'WFGA', 'WFGM3', 'WFGA3', 'WFTM', 'WFTA', 'WOR', 'WDR', 'WAst', 'WTO', 'WStl', 'WBlk', 'WPF'], axis=1, inplace=True)
+	# Drop original measures
+	df.drop(['WFGM', 'WFGA', 'WFGM3', 'WFGA3', 'WFTM', 'WFTA', 'WOR', 'WDR', 'WAst', 'WTO', 'WStl', 'WBlk', 'WPF'],axis=1, inplace=True)
 	df.drop(['LFGM', 'LFGA', 'LFGM3', 'LFGA3', 'LFTM', 'LFTA', 'LOR', 'LDR', 'LAst', 'LTO', 'LStl', 'LBlk', 'LPF'], axis=1, inplace=True)
-
+	df.drop(['WLoc'], axis=1, inplace=True)
+	# take mean, min, max of each of the advanced measures
+	W_cols  = [col for col in df if col.startswith('W') and col not in ["WTeamID","WScore"]]
+	L_cols = [col for col in df if col.startswith('L') and col not in ["LTeamID", "LScore"]]
+	# make win and loss average datasets
+	Wmean = df.groupby(['WTeamID', 'Season'])[W_cols].mean()
+	Wmean.columns = [x.replace('W', '') for x in Wmean.columns]
+	Wmean.columns = [x+"_mean" for x in Wmean.columns]
+	Wmean['TeamID'] = Wmean.index.get_level_values('WTeamID')
+	Wmean['Season'] = Wmean.index.get_level_values('Season')
+	# loss
+	Lmean = df.groupby(['LTeamID', 'Season'])[L_cols].mean()
+	Lmean.columns = [x.replace('L', '') for x in Lmean.columns]
+	Lmean.columns = [x+"_mean" for x in Lmean.columns]
+	Lmean['TeamID'] = Lmean.index.get_level_values('LTeamID')
+	Lmean['Season'] = Lmean.index.get_level_values('Season')
+	# concatenate both and take average over team and season
+	fdat = pd.concat([Wmean, Lmean], axis=0, sort=True)
+	cols_to_get = [x for x in fdat.columns if x not in ["TeamID", "Season"]]
+	fdat_mean = fdat.groupby(['TeamID', 'Season'])[cols_to_get].mean()
+	fdat_mean['TeamID'] = fdat_mean.index.get_level_values('TeamID')
+	fdat_mean['Season'] = fdat_mean.index.get_level_values('Season')
+	# create two
+	Wfdat = fdat_mean.copy()
+	Wfdat.columns = ["W"+x if x not in "Season" else x for x in Wfdat.columns]
+	Lfdat = fdat_mean.copy()
+	Lfdat.columns = ["L"+x if x not in "Season" else x for x in Lfdat.columns]
 	# NEED TO MAKE THIS COMPATIBLE WITH THE REST OF THE DATA: TAKE DIFFS AND CONCATENATE
+	df_tour = pd.read_csv('data/DataFiles/NCAATourneyCompactResults.csv')
+	df_tour.drop(labels=['DayNum', 'WScore', 'LScore', 'WLoc', 'NumOT'], inplace=True, axis=1)
+	df = pd.merge(left = df_tour, right = Wfdat, how = 'left', on = ['Season', 'WTeamID'])
+	df = pd.merge(left = df, right = Lfdat, how = 'left', on = ['Season', 'LTeamID'])
+
 	df_concat = pd.DataFrame()
-	vars_to_add =  set([x[1:] for x in df.columns[11:]])
+	vars_to_add =  [x.replace("L", "") for x in Lfdat.columns if x not in ["Season", "WTeamID", "LTeamID"]]
 	for var in vars_to_add:
 		df_concat['Diff_'+var] = df['W'+var]-df['L'+var]
-	# 
-	df_win = df[['Season', 'WTeamID', 'LTeamID']]
-	df_win = pd.concat([df_win, df_concat], axis=1)
-	df_win['Result'] = 1
 
-	df_loss = df[['Season', 'WTeamID', 'LTeamID']]
-	df_loss = pd.concat([df_loss, -df_concat], axis=1)
-	df_loss['Result'] = 0
+	pred_vars = df_concat.columns 
+	df_concat["WTeamID"] = df.WTeamID
+	df_concat["LTeamID"] = df.LTeamID
+	df_concat["Season"] = df.Season
 
-	df_out = pd.concat((df_win, df_loss))
+	df_wins = pd.DataFrame()
+	df_wins = df_concat.copy()
+	df_wins['Result'] = 1
+
+	df_losses = pd.DataFrame()
+	df_losses = df_concat[['Season', 'WTeamID', 'LTeamID']]
+	df_losses[pred_vars] = -df_concat[pred_vars].copy()
+	df_losses['Result'] = 0
+
+	df_out = pd.concat((df_wins, df_losses))
 
 	print("done")
 	return(df_out)
--- notebooks/pipeline.py
+++ notebooks/pipeline.py
@@ -1,4 +1,9 @@
 import os
+import pandas as pd
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
+from sklearn.svm import LinearSVC
 os.chdir('/Users/electron/Documents/mm/notebooks')
 from eff_stats import *
 from elo_ranks import *
@@ -15,8 +20,6 @@ seeds = make_seeds()
 	# efficiency stats
 adf = eff_stats()
 
-	# historic match-ups: what was the score differential the last time this couple met in the tourney?
-
 	# elo rankings
 elo_obj = elo() #it's a class, so you have to instantiate
 elo_df = elo_obj.elo_ranks()
@@ -27,6 +30,41 @@ full = pd.merge(left=full, right=elo_df, how='left', on=['Season', 'WTeamID', 'L
 full = full.dropna()
 
 # Create cross-validation setup (integrate some ideas from Lopez del Prado in purged k-fold CV)
+ntrain = 1572
+X_full = full.drop(['Result', 'WTeamID', 'LTeamID', 'Season'], axis=1).values
+y_full = full.Result.values
+X_full, y_full = shuffle(X_full, y_full)
+X_train = X_full[:ntrain, :]
+y_train = y_full[:ntrain]
+
+# Create validation dataset
+X_val = X_full[ntrain:, :]
+y_val = y_full[ntrain:]
+
+# DO SOME MODELLING WITH TRAINING DATA:::
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+
+# SVM
+clf = LinearSVC(random_state=0, tol=1e-5, penalty='l2', max_iter=5000)
+#clf = CalibratedClassifierCV(clf) 
+n_sims = 200
+y_pred_svm = cross_val_predict(clf, X_train, y_train, cv=3, method='predict_proba')
+#accuracy_score(y_train, y_pred_svm)
+#y_pred_svm_prob = [ [x[0], 1-x[0]] for x in y_pred_svm]
+log_loss(y_true = y_train, y_pred = y_pred_svm)
+
+# LOGIT
+logreg = LogisticRegression()
+params = {'C': np.logspace(start=-5, stop=3, num=9)}
+clf = GridSearchCV(logreg, params, scoring='neg_log_loss', refit=True)
+clf.fit(X_train, y_train)
+preds = [ [x[0], 1-x[0]] for x in clf.predict_proba(X_train)]
+log_loss(y_true = y_train, y_pred = preds)
+
+# NOT UNTIL THE VERY END!!!
+preds = [ [x[0], 1-x[0]] for x in clf.predict_proba(X_test)]
+log_loss(y_true = y_test, y_pred = preds)
 
 
 # Create final matchups, write to CSV
